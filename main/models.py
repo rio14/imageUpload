c67a62ac33cf5e9ca7e74f6318d5cdbe90acf0c4
rio14 <riteshsinha14.rs@gmail.com>
2020-06-23 17:35:35 +0530

    Update models.py

--- main/models.py
+++ main/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class uploadFiles(models.Model):
-    fname = models.CharField()
+    fname = models.CharField(max_length=100)
     up_date = models.DateTimeField(auto_now=True)
     fileL = models.FileField() 
